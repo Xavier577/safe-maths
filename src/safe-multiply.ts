a27b86c9693ecf5a51cd9a793014e4d630e99d57
Joseph Tsegen <tsegstech10@gmail.com>
2024-04-02 17:28:53 +0100

    corrected safeMultiply Typo

--- src/safe-multiply.ts
+++ src/safe-multiply.ts
@@ -2,7 +2,7 @@ import { decimalCount } from "./decimal-count";
 
 export function safeMultiply(a: number, b: number): number {
   const aMultiplier = Math.pow(10, decimalCount(a));
-  const bMultiplier = Math.pow(10, decimalCount(a));
+  const bMultiplier = Math.pow(10, decimalCount(b));
 
   const aInt = a * aMultiplier;
   const bInt = b * bMultiplier;
